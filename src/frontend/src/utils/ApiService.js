05247cfcccae4c812470b04fa88f57603886c4e3
Ronanterb <teyouronan@gmail.com>
2023-01-18 10:28:43 +0100

    putting the ip inside gitignore

--- src/frontend/src/utils/ApiService.js
+++ src/frontend/src/utils/ApiService.js
@@ -1,7 +1,14 @@
 // import axios from 'axios'
 
+// to import endPoint create a js file inside the utils folder named EndUrl.js
+// then paste the text below :
+// export const endPoint = `http://{ip}:8000`;
+// NB: replace {ip} with your actual ip address
+
+import { endPoint } from "./EndUrl";
+
 export default class Trixwallet {
-  static endPoint = 'http://192.168.8.101:8000'
+  static endPoint = endPoint; 
   static webSocketUrl = 'ws://127.0.0.1:8000';
 
   static async getLatestTransactions(token){
